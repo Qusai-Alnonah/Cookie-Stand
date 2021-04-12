e15bc97c47f9d13ef118eb9d5f5391ff7038d76b
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-04-12 08:58:07 +0300

    not finsh

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-'use strict';
+'use strict'
+let hours = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
+let salesInput = document.getElementById('salesInput');
 const seaitel ={
    minCust:23,
    maxCust:65 ,
@@ -8,18 +10,4 @@ const seaitel ={
     min = Math.ceil(23);
     max = Math.floor(65);
     return Math.floor(Math.random() * (65 - 23 + 1) + 23); //The maximum is inclusive and the minimum is inclusive
-   } 
-   randomnumberberHour: function(){
-    
-      randomNum =[];
-       this.randomNum  
-      for (i=0;i<=randomNum.length;i++)
-      let hour=[6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-  let CookesberHour = [];
-      }
-  calculatCookies : function(cookeaberHour){
-
-
-  }
-
-}
+   }}
